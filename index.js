8c120bee7ab7f7155658c5b868a2946d9b2994bc
Brittany Malooly <bmalooly@gmail.com>
2018-12-31 17:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 function happyHolidays(string) {return "Happy holidays!"}
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
 function happyHolidayTo(holiday,name) {return `Happy ${holiday}, ${name}!`}
-function holidayCountdown(string) {return `It's ${days} days until ${holiday}!`}
+function holidayCountdown(holiday,days) {return `It's ${days} days until ${holiday}!`}
